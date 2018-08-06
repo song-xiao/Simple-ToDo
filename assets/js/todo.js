c3d7c3781fdbc617234b323743f3d3610dec0793
Song Xiao <sxa17@sfu.ca>
2018-08-06 14:57:55 -0700

    Implement todo.js to support basic functionality

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,1 +1,25 @@
-alert("Connected!");
+// Check off specific Todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete corresponding Todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Create new Todo
+$("input[type='text']").on("keypress",function(event){
+    var todoText;
+    // enter Key pressed
+    if (event.which === 13){
+        todoText = $(this).val();
+        // reset input field
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
